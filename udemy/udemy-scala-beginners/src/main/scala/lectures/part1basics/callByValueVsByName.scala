d26cc428f8dc2baf89d4c6ff7463a1683e9cf249
undersfx <atchin_@hotmail.com>
2023-03-25 12:13:16 -0300

    add: call by value vs call by name lecture

---
+++ udemy/udemy-scala-beginners/src/main/scala/lectures/part1basics/callByValueVsByName.scala
@@ -0,0 +1,23 @@
+package lectures.part1basics
+
+object callByValueVsByName extends App {
+  def calledByValue(x: Long): Unit = {  // expresion x is evaluated before the call
+    println("by value call: " + x)
+    println("by value call: " + x)
+  }
+
+  def calledByName(x: => Long): Unit = {  // expression x is evaluated only when needed
+    println("by name call: " + x)
+    println("by name call: " + x)
+  }
+
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+
+  // eager vs lazy evaluation example
+  def infinite(): Int = 1 + infinite()
+  def printFirst(eagerArg: Int, lazyArg: => Int) = println(eagerArg)
+
+  printFirst(eagerArg = 42, lazyArg = infinite())   // 42
+  printFirst(eagerArg = infinite(), lazyArg = 42)   // java.lang.StackOverflowError
+}
