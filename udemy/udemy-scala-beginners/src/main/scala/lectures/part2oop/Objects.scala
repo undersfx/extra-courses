b98e246e9dfaf261cbc597ec3f29380f1367bd6a
undersfx <atchin_@hotmail.com>
2023-04-11 21:52:49 -0300

    add: lecture 14 playground

---
+++ udemy/udemy-scala-beginners/src/main/scala/lectures/part2oop/Objects.scala
@@ -0,0 +1,46 @@
+package lectures.part2oop
+
+object Objects {
+
+  // SCALA DOES NOT HAVE CLASS-LEVEL FUNCTIONALITY
+
+  object Person {
+    // static or class functionality goes on object
+    val N_EYES: Int = 2
+    val canFly: Boolean = false
+    def apply(mother: Person, father: Person): Person = new Person("Bobby")
+  }
+
+  class Person(val name: String) {
+    // instance-level functionality
+    def greet(): String = s"Hi, my name is $name"
+  }
+  // those two Person definitions together are called COMPANIONS
+
+
+  // Scala objects are SINGLETONS
+  val mary = Person
+  val john = Person
+  println(s"Is object mary equals john ? ${mary == john}")
+
+  // Class instances are not SINGLETONS
+  val gwen = new Person("Gwen")
+  val joe = new Person("Joe")
+  println(s"Is class instance gwen equals joe ? ${gwen == joe}")
+
+  // class functionality access
+  println(Person.N_EYES)
+  // instance functionality access
+  println(gwen.greet())
+
+  // factory method using apply syntatic sugar
+  val boobie = Person(gwen, joe)
+  println(boobie.greet())
+
+
+  // Scala Applications (extends App) is a object with the method:
+  // def main(args: Array[String]): Unit
+  def main(args: Array[String]): Unit = {
+    println("hello from main method")
+  }
+}
