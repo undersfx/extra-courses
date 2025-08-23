bd5e7eb6474eda74d4dfef6762ad0a2ab8de9914
undersfx <atchin_@hotmail.com>
2025-08-23 18:53:08 -0300

    refactor: fix SocialNetwork.remove functionality

--- udemy/udemy-scala-beginners/src/main/scala/lectures/part3functional/TuplesAndMaps.scala
+++ udemy/udemy-scala-beginners/src/main/scala/lectures/part3functional/TuplesAndMaps.scala
@@ -5,8 +5,8 @@ import scala.annotation.tailrec
 object TuplesAndMaps extends App {
   // Tuples - finite ordered "lists"
 
-  val aTuple       = new Tuple2(2, "Foo")
-  val otherTuple   = Tuple2(2, "Foo")
+  val aTuple = new Tuple2(2, "Foo")
+  val otherTuple = Tuple2(2, "Foo")
   val anotherTuple = (2, "Foo")
   // (Int, String) is syntactic sugar for Tuple2[Int, String]
   // Limited to 22 element types: Tuple1 to Tuple22 (same limit in Function types)
@@ -56,78 +56,85 @@ object TuplesAndMaps extends App {
 
   // Conversions to other collections
   println(phoneBook.toList)
-  println(List(("Daniel",123), ("Jim",555)).toMap)
+  println(List(("Daniel", 123), ("Jim", 555)).toMap)
 
   val names = List("Ana", "Bob", "Angela", "Brian")
   println(names.groupBy(name => name.charAt(0)))
 
+
   // Social Network Exercise
-    case class SocialNetwork(population: Map[String, List[String]] = Map()) {
-      def add(p: String) = SocialNetwork(this.population + (p -> List()))
+  case class SocialNetwork(population: Map[String, List[String]] = Map()) {
+    def add(p: String) = SocialNetwork(this.population + (p -> List()))
 
-      def remove(p: String) = SocialNetwork(this.population.removed(p))
-
-      def friend(p: String, f: String) = {
-        val pNewPair = p -> (this.population(p) :+ f)
-        val fNewPair = f -> (this.population(f) :+ p)
-        SocialNetwork(this.population + pNewPair + fNewPair)
-      }
-
-      def unfriend(p: String, f: String) = {
-        val pNewPair = p -> (this.population(p).filter(_ != f))
-        val fNewPair = f -> (this.population(f).filter(_ != p))
-
-        SocialNetwork(this.population + pNewPair + fNewPair)
-      }
-
-      def numberOfFriends(p: String): Int = this.population(p).size
-
-      def mostFriends: String = {
-        this.population.reduce(
-          (a, b) => {
-            if a._2.size > b._2.size then a
-            else b
-          }
-        )._1
-      }
-
-      def noFriendsCount: Int = {
-        this.population.count(_._2.isEmpty)
-      }
-
-      def connectionBetween(p: String, f: String): Boolean = {
-        def inner(current: String, pf: List[String], f: String, visited: List[String]): Boolean = {
-          if (pf.isEmpty) false
-          else if (pf.contains(f)) true
-          else {
-            pf.exists(next =>
-              inner(next, this.population(next).filterNot(visited.contains), f, visited :+ current)
-            )
-          }
-        }
-
-        inner(p, this.population(p), f, List())
-      }
+    def remove(p: String) = {
+      SocialNetwork(
+        this.population
+          .removed(p)
+          .view.mapValues(f => f.filter(_ != p)).toMap
+      )
     }
 
+    def friend(p: String, f: String) = {
+      val pNewPair = p -> (this.population(p) :+ f)
+      val fNewPair = f -> (this.population(f) :+ p)
+
+      SocialNetwork(this.population + pNewPair + fNewPair)
+    }
+
+    def unfriend(p: String, f: String) = {
+      val pNewPair = p -> (this.population(p).filter(_ != f))
+      val fNewPair = f -> (this.population(f).filter(_ != p))
+      SocialNetwork(this.population + pNewPair + fNewPair)
+    }
+
+    def numberOfFriends(p: String): Int = this.population(p).size
+
+    def mostFriends: String = {
+      this.population.reduce(
+        (a, b) => {
+          if a._2.size > b._2.size then a
+          else b
+        }
+      )._1
+    }
+
+    def noFriendsCount: Int = {
+      this.population.count(_._2.isEmpty)
+    }
+
+    def connectionBetween(p: String, f: String): Boolean = {
+      def inner(current: String, pf: List[String], f: String, visited: List[String]): Boolean = {
+        if (pf.isEmpty) false
+        else if (pf.contains(f)) true
+        else {
+          pf.exists(next =>
+            inner(next, this.population(next).filterNot(visited.contains), f, visited :+ current)
+          )
+        }
+      }
+
+      inner(p, this.population(p), f, List())
+    }
+  }
 
 
-    val s = SocialNetwork()
-      .add("Thiago")
-      .add("Emily")
-      .add("Cacau")
-      .add("Max")
-      .add("Gaspar")
-      .friend("Thiago", "Emily")
-      .friend("Thiago", "Max")
-      .friend("Emily", "Cacau")
-      .friend("Cacau", "Max")
-      .unfriend("Max", "Cacau")
+  val s = SocialNetwork()
+    .add("Thiago")
+    .add("Emily")
+    .add("Cacau")
+    .add("Max")
+    .add("Gaspar")
+    .friend("Thiago", "Emily")
+    .friend("Thiago", "Max")
+    .friend("Emily", "Cacau")
+    .friend("Cacau", "Max")
+    .unfriend("Max", "Cacau")
 
-    println(s)
-    println(s.numberOfFriends("Thiago"))
-    println(s.mostFriends)
-    println(s.noFriendsCount)
-    println(s.connectionBetween("Thiago", "Cacau"))
-    println(s.connectionBetween("Thiago", "Gaspar"))
+  println(s)
+  println(s.numberOfFriends("Thiago"))
+  println(s.mostFriends)
+  println(s.noFriendsCount)
+  println(s.connectionBetween("Thiago", "Cacau"))
+  println(s.connectionBetween("Thiago", "Gaspar"))
+  println(s.remove("Thiago"))
 }
